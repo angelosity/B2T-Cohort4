a4a98ae4e336c5fe700fd2a4cb8338f7b0e13ede
angelosity <angelmontilla12@hotmail.com>
2018-11-08 09:53:55 -0500

    folder added

--- array-loop/script.js
+++ array-loop/script.js
@@ -1,12 +1,24 @@
-function fizzBuzz(){
-	for(var i=1;i<=100;i++){
-		if(i%3 === 0){
-			console,log('Fizz');
-		} 
-		if(i%5 === 0){
-			console,log('Buzz');
-		} 
-		if(i%3 !== 0 && i%5 !== 0) {
-			console,log(i);
+let color =["Blue ", "Green ", "Red ", "Orange ", "Violet ", "Indigo", "Yellow"];
+
+let o = [ "th", "st", "nd", "rd"];
+
+   for (let i=0; i < color.length; i++){
+		
+		if (i ===0){ 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[1] + " choice is" + color[i]+ "<hr/>";
+
 		}
-	}
+		else if (i ===1){ 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[2] + " choice is" + color[i] + "<hr/>";
+		}
+		else if (i ===2){ 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[3] + " choice is" + color[i]+ "<hr/>";
+		}
+		else { 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[0] + " choice is" + color[i]+ "<hr/>";
+		}
+	} 
+
+
+   
+
